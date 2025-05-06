2cf6b9c74f12a2e6b8f11ea9af3e0f335e652561
Isabell Haliunaa Arnke <s0587688@htw-berlin.de>
2025-05-06 11:18:43 +0200

    Testactor Tracking

--- CaveSoSe2025/Assets/PositionTransfer.cs
+++ CaveSoSe2025/Assets/PositionTransfer.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class PositionTransfer : MonoBehaviour
 {
+    private GameObject actor;
+
     // Start is called before the first frame update
     void Start()
     {
-        GameObject actor = GameObject.FindGameObjectWithTag("TestActor");
-        if (!actor)
-        {
-            Debug.LogError("Testactor nicht gefunden");
-        }
+        actor = GameObject.FindGameObjectWithTag("Player");
 
+        if (actor == null)
+        {
+            Debug.LogError("Player nicht gefunden!");
+        }
+        else
+        {
+            Debug.Log("Player Position: " + actor.transform.position);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (actor != null)
+        {
+            Vector3 position = actor.transform.position;
+            Debug.Log("Aktuelle Position von Player: " + position);
+        }
     }
 }
