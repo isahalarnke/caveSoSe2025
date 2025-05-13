53db5c3308306c7d01eb5cca564847b05cd1529d
Isabell Haliunaa Arnke <s0587688@htw-berlin.de>
2025-05-13 11:47:50 +0200

    Joints Positions Transfer + Test Lines between

--- CaveSoSe2025/Assets/PositionTransfer.cs
+++ CaveSoSe2025/Assets/PositionTransfer.cs
@@ -6,14 +6,29 @@ public class PositionTransfer : MonoBehaviour
 {
     private GameObject actor;
     private Transform head;
+    private Transform neck;
     private Transform hipLeft;
     private Transform hipRight;
     private Transform spineBase;
+    private Transform spineMid;
+    private Transform shoulderLeft;
+    private Transform shoulderRight;
+    private Transform elbowLeft;
+    private Transform elbowRight;
+    private Transform handLeft;
+    private Transform handRight;
+    private Transform kneeLeft;
+    private Transform kneeRight;
+    private Transform ankleLeft;
+    private Transform ankleRight;
+    private Transform footLeft;
+    private Transform footRight;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // In most cases this doesn't work because the TestActor is instantiated after Start() method is called
+
         actor = GameObject.FindGameObjectWithTag("Player");
         if (actor != null)
         {
@@ -22,7 +37,7 @@ public class PositionTransfer : MonoBehaviour
         
     }
 
-    // Update is called once per frame
+    // Finding the instantiated TestActor 2 Object with the Tag "Player" and transfer the joint positions to cubes, that are instantiated. --> special features for a certain body part in planning
     void Update()
     {
        
@@ -33,31 +48,78 @@ public class PositionTransfer : MonoBehaviour
             {
                 Debug.Log("Player gefunden und Position: " + actor.transform.position);
 
+                // Finding the positions of the TestActot joints
 
                 head = actor.transform.Find("Spine Base/Spine Mid/Spine Shoulder/Neck/Head");
+                neck = actor.transform.Find("Spine Base/Spine Mid/Spine Shoulder/Neck");
+                spineBase = actor.transform.Find("Spine Base");
+                spineMid = actor.transform.Find("Spine Base/Spine Mid")
                 hipRight = actor.transform.Find("Spine Base/Hip Right");
                 hipLeft = actor.transform.Find("Spine Base/Hip Left");
-                spineBase = actor.transform.Find("Spine Base");
+                kneeLeft = actor.transform.Find("Spine Base/Hip Left/Knee Left");
+                kneeRight = actor.transform.Find("Spine Base/Hip Right/Knee Right");
+                ankleLeft = actor.transform.Find("Spine Base/Hip Left/Knee Left/Ankle Left");
+                ankleRight = actor.transform.Find("Spine Base/Hip Right/Knee Right/Ankle Right");
+                footLeft = actor.transform.Find("Spine Base/Hip Left/Knee Left/Ankle Left/Foot Left");
+                footRight = actor.transform.Find("Spine Base/Hip Right/Knee Right/Ankle Right/Foot Right");
+                shoulderLeft = actor.transform.Find("Spine Base/Spine Mid/Spine Shoulder/Shoulder Left");
+                shoulderRight = actor.transform.Find("Spine Base/Spine Mid/Spine Shoulder/Shoulder Right");
+                elbowLeft = actor.transform.Find("Spine Base/Spine Mid/Spine Shoulder/Shoulder Left/Elbow Left");
+                elbowRight = actor.transform.Find("Spine Base/Spine Mid/Spine Shoulder/Shoulder Right/Elbow Right");
+                handLeft = actor.transform.Find("Spine Base/Spine Mid/Spine Shoulder/Shoulder Left/Elbow Left/Wrist Left/Hand Left");
+                handRight = actor.transform.Find("Spine Base/Spine Mid/Spine Shoulder/Shoulder Right/Elbow Right/Wrist Right/Hand Right");
 
-                if (head != null)
-                {
-                    CreateCubeAtPosition(head.position, "HeadCube", head);
-                }
+                // Creating cubes with offset to visualize a character
 
-                if (hipRight != null)
-                {
-                    CreateCubeAtPosition(hipRight.position, "HipRightCube", hipRight);
-                }
+                if (head != null) CreateCubeAtPosition(head.position, "HeadCube", head);
+                if (neck != null) CreateCubeAtPosition(neck.position, "NeckCube", neck);
+                if (spineBase != null) CreateCubeAtPosition(spineBase.position, "SpineBaseCube", spineBase);
+                if (hipLeft != null) CreateCubeAtPosition(hipLeft.position, "HipLeftCube", hipLeft);
+                if (hipRight != null) CreateCubeAtPosition(hipRight.position, "HipRightCube", hipRight);
+                if (kneeLeft != null) CreateCubeAtPosition(kneeLeft.position, "KneeLeftCube", kneeLeft);
+                if (kneeRight != null) CreateCubeAtPosition(kneeRight.position, "KneeRightCube", kneeRight);
+                if (ankleLeft != null) CreateCubeAtPosition(ankleLeft.position, "AnkleLeftCube", ankleLeft);
+                if (ankleRight != null) CreateCubeAtPosition(ankleRight.position, "AnkleRightCube", ankleRight);
+                if (footLeft != null) CreateCubeAtPosition(footLeft.position, "FootLeftCube", footLeft);
+                if (footRight != null) CreateCubeAtPosition(footRight.position, "FootRightCube", footRight);
+                if (shoulderLeft != null) CreateCubeAtPosition(shoulderLeft.position, "ShoulderLeftCube", shoulderLeft);
+                if (shoulderRight != null) CreateCubeAtPosition(shoulderRight.position, "ShoulderRightCube", shoulderRight);
+                if (elbowLeft != null) CreateCubeAtPosition(elbowLeft.position, "ElbowLeftCube", elbowLeft);
+                if (elbowRight != null) CreateCubeAtPosition(elbowRight.position, "ElbowRightCube", elbowRight);
+                if (handLeft != null) CreateCubeAtPosition(handLeft.position, "HandLeftCube", handLeft);
+                if (handRight != null) CreateCubeAtPosition(handRight.position, "HandRightCube", handRight);
 
-                if (hipLeft != null)
-                {
-                    CreateCubeAtPosition(hipLeft.position, "HipLeftCube", hipLeft);
-                }
+                //Drawing lines between specific characters
 
-                if (spineBase != null)
-                {
-                    CreateCubeAtPosition(spineBase.position, "SpineBaseCube", spineBase);
-                }
+                // head and Upperbody
+                if (neck != null && head != null) DrawLineBetweenJoints(neck, head, "Line_Neck_Head");
+                if (spineMid != null && neck != null) DrawLineBetweenJoints(spineMid, neck, "Line_SpineMid_Neck");
+                if (spineBase != null && spineMid != null) DrawLineBetweenJoints(spineBase, spineMid, "Line_SpineBase_SpineMid");
+
+                // Shoulders and arms
+                if (spineMid != null && shoulderLeft != null) DrawLineBetweenJoints(spineMid, shoulderLeft, "Line_SpineMid_ShoulderLeft");
+                if (spineMid != null && shoulderRight != null) DrawLineBetweenJoints(spineMid, shoulderRight, "Line_SpineMid_ShoulderRight");
+
+                if (shoulderLeft != null && elbowLeft != null) DrawLineBetweenJoints(shoulderLeft, elbowLeft, "Line_ShoulderLeft_ElbowLeft");
+                if (shoulderRight != null && elbowRight != null) DrawLineBetweenJoints(shoulderRight, elbowRight, "Line_ShoulderRight_ElbowRight");
+
+                if (elbowLeft != null && handLeft != null) DrawLineBetweenJoints(elbowLeft, handLeft, "Line_ElbowLeft_HandLeft");
+                if (elbowRight != null && handRight != null) DrawLineBetweenJoints(elbowRight, handRight, "Line_ElbowRight_HandRight");
+
+                // Hips, Legs(Knee, Ankle, Feet)
+                if (spineBase != null && hipLeft != null) DrawLineBetweenJoints(spineBase, hipLeft, "Line_SpineBase_HipLeft");
+                if (spineBase != null && hipRight != null) DrawLineBetweenJoints(spineBase, hipRight, "Line_SpineBase_HipRight");
+
+                if (hipLeft != null && kneeLeft != null) DrawLineBetweenJoints(hipLeft, kneeLeft, "Line_HipLeft_KneeLeft");
+                if (hipRight != null && kneeRight != null) DrawLineBetweenJoints(hipRight, kneeRight, "Line_HipRight_KneeRight");
+
+                if (kneeLeft != null && ankleLeft != null) DrawLineBetweenJoints(kneeLeft, ankleLeft, "Line_KneeLeft_AnkleLeft");
+                if (kneeRight != null && ankleRight != null) DrawLineBetweenJoints(kneeRight, ankleRight, "Line_KneeRight_AnkleRight");
+
+                if (ankleLeft != null && footLeft != null) DrawLineBetweenJoints(ankleLeft, footLeft, "Line_AnkleLeft_FootLeft");
+                if (ankleRight != null && footRight != null) DrawLineBetweenJoints(ankleRight, footRight, "Line_AnkleRight_FootRight");
+
+
             }
         }
 
@@ -70,7 +132,7 @@ public class PositionTransfer : MonoBehaviour
         }
     }
 
-    // Kreiere Würfel zum Testen an den Positionen um zu Testen, ob die Positionen tracken 
+    // Cubed at the position with offset of the TestActor to visualize a character
 
     private void CreateCubeAtPosition(Vector3 position, string cubeName, Transform parent)
     {
@@ -89,5 +151,25 @@ public class PositionTransfer : MonoBehaviour
         Renderer cubeRenderer = cube.GetComponent<Renderer>();
 
         cubeRenderer.material.color = new Color(1f, 0.41f, 0.71f);
+    }
+
+    // Drawing lines with a Line Renderer object between correct joints
+
+    private void DrawLineBetweenJoints(Transform start, Transform end, string lineName)
+    {
+        GameObject lineObj = new GameObject(lineName);
+        LineRenderer lineRenderer = lineObj.AddComponent<LineRenderer>();
+
+        lineRenderer.positionCount = 2;
+        lineRenderer.SetPosition(0, start.position);
+        lineRenderer.SetPosition(1, end.position);
+
+        lineRenderer.startWidth = 0.02f;
+        lineRenderer.endWidth = 0.02f;
+
+        // Materials 
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.startColor = Color.green;
+        lineRenderer.endColor = Color.green;
     }
 }
